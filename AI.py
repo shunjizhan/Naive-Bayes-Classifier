8a7d302faa7452b2d8d9d603eb57356b7e4388b7
shunjizhan <shunjizhan@umail.ucsb.edu>
2017-02-14 13:21:34 -0800

    denoise works

--- AI.py
+++ AI.py
@@ -1,10 +1,28 @@
 import sys
 
-for arg in sys.argv:
-    print arg
-
 def readFile(filename):
-	lines = [line.rstrip('\n').split('\t') for line in open(filename)]
+	lines = [line.rstrip('\n').lower() for line in open(filename)]
 	return lines
 
-print readFile(sys.argv[1])
+def deNoiseSentence(sentence):
+	noise = ['the', 'a', 'an', 'i', 'that']
+
+	words = sentence.split()
+	resultwords  = [word for word in words if word not in noise]
+	result = ' '.join(resultwords)
+
+	return result
+
+
+def deNoise(file):	 
+	result = [deNoiseSentence(sentence) for sentence in file]
+	return result
+
+
+
+
+
+collection = readFile(sys.argv[1])	# contain paragrahs list 
+collection = deNoise(collection)	
+collection = [item.split('\t') for item in collection]	# now list of [{comment}, {score}]
+print collection
