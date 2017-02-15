63656e06fa4567028af2a1b01c2828231552676a
shunjizhan <shunjizhan@umail.ucsb.edu>
2017-02-14 17:14:28 -0800

    data problem?

--- AI.py
+++ AI.py
@@ -1,4 +1,17 @@
 import sys
+import math
+
+global frequencyLib
+global frequencyLib1
+global frequencyLib0
+
+global total
+global total1
+global total0
+
+global totalScore
+global pos
+global neg
 
 def readFile(filename):
 	lines = [line.rstrip('\n').lower() for line in open(filename)]
@@ -57,19 +70,30 @@ def countTotal(lib):
 	return sum
 
 def predict(comment):	# comment: [  ['a','b'] , ['c','c'] , ['d','e']  ]
-	pos = 0
-	neg = 0
-	total = countTotal(frequencyLib)
-	total1 = countTotal(frequencyLib1)
-	total0 = countTotal(frequencyLib0)
+	# positive = math.log(pos, 2)
+	# negative = math.log(neg, 2)	
+	p = pos
+	n = neg
+	positive = 0
+	negative = 0
+	for word in comment:
+		if (frequencyLib.has_key(word)):
+			pWord1 = float(frequencyLib[word]) / total1
+			pWord0 = float(frequencyLib[word]) / total0
+			# positive += math.log(pWord1, 2) 
+			# negative += math.log(pWord0, 2)			
+			positive += pWord1
+			negative += pWord0
 
-	totalScore = float(frequencyLib['1']) + float(frequencyLib['0'])
-	p1 = float(frequencyLib['1']) / totalScore
-	p0 = float(frequencyLib['0']) / totalScore
+	positive = p * positive
+	negative = n * negative
 
-	print p1 + p0
-	
-	return 0
+	print positive, negative
+
+	if(positive > negative):
+		return 1
+	else:
+		return 0	
 
 
 def test():
@@ -81,7 +105,7 @@ def test():
 		comment.append(oneComment[:-1])
 		realScore.append(int(oneComment[-1]))
 		predictScore.append(predict(oneComment[:-1]))
-	print 'commnet: ', comment		
+	# print 'commnet: ', comment		
 	print 'realScore: ', realScore
 	print 'predictsc: ', predictScore
 
@@ -90,14 +114,20 @@ def test():
 trainData = deNoise(readFile(sys.argv[1]))	# [   ['a','b','1'] , ['c','c','1'] , ['d','e','1']   ]
 #collection = deNoise(collection)	# [   [['a','b'],'1'] , [['c','c'],'1'] , [['d','e'],'1']   ]
 
-global frequencyLib
-global frequencyLib1
-global frequencyLib0
+
 
 frequencyLib = buildLib(trainData)			# print frequencyLib.items()
 freqLibSep = seperateData(trainData)
 frequencyLib1 = buildLib(freqLibSep[0])
 frequencyLib0 = buildLib(freqLibSep[1])
+
+total = countTotal(frequencyLib)
+total1 = countTotal(frequencyLib1)
+total0 = countTotal(frequencyLib0)
+
+totalScore = float(frequencyLib['1']) + float(frequencyLib['0'])
+pos = float(frequencyLib['1']) / totalScore		# p(1)
+neg = float(frequencyLib['0']) / totalScore		# p(0)
 
 test()
 
